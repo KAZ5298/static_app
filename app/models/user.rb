fb2f6aba001a539a85855b383241b3a6ad03a558
EC2 Default User <ec2-user@ip-172-31-39-49.ap-northeast-1.compute.internal>
2021-03-27 10:21:51 +0000

    登録情報条件追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :name, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+  validates :email, format: { with: VALID_EMAIL_REGEX }
 end
